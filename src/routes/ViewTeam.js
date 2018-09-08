4ab2e4f5f5ac73e8a120b37f1e515db2f5ee489a
SabithHaneef <sabith.th.07@gmail.com>
2018-09-08 18:07:23 +0530

    lift up gql allTeamsQuery to ViewTeam to share data across components

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import findIndex from 'lodash/findIndex';
 import AppLayout from '../components/AppLayout';
 import Messages from '../components/Messages';
 import Header from '../components/Header';
 import SendMessage from '../components/SendMessage';
 import Sidebar from '../containers/Sidebar';
+import { allTeamsQuery } from '../graphql/team';
 
-const ViewTeam = ({ match: { params } }) => (
-  <AppLayout>
-    <Sidebar currentTeamId={params.teamId} />
-    <Header channelName="general" />
-    <Messages>
-      <ul className="message-list">
-        <li>Item 1</li>
-        <li>Item 2</li>
-      </ul>
-    </Messages>
-    <SendMessage channelName="general" />
-  </AppLayout>
-);
+const ViewTeam = ({
+  data: { loading, allTeams },
+  match: {
+    params: { teamId, channelId },
+  },
+}) => {
+  if (loading) {
+    return null;
+  }
 
-export default ViewTeam;
+  const teamIndex = teamId ? findIndex(allTeams, ['id', parseInt(teamId, 10)]) : 0;
+  const team = allTeams[teamIndex];
+  const channelIndex = channelId ? findIndex(team.channels, ['id', parseInt(channelId, 10)]) : 0;
+  const channel = team.channels[channelIndex];
+
+  return (
+    <AppLayout>
+      <Sidebar
+        teams={allTeams.map(t => ({
+          id: t.id,
+          initial: t.name.charAt(0).toUpperCase(),
+        }))}
+        team={team}
+      />
+      <Header channelName={channel.name} />
+      <Messages channelId={channel.id}>
+        <ul className="message-list">
+          <li>Item 1</li>
+          <li>Item 2</li>
+        </ul>
+      </Messages>
+      <SendMessage channelName={channel.name} />
+    </AppLayout>
+  );
+};
+
+export default graphql(allTeamsQuery)(ViewTeam);
