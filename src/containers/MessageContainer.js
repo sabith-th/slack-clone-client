a91b8782fa8d5b0228913cf3ff1ebf1fef2bcab6
SabithHaneef <sabith.th.07@gmail.com>
2018-09-11 22:28:07 +0530

    use semantic ui Comment to display messages

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Comment, Header } from 'semantic-ui-react';
 import Messages from '../components/Messages';
 
-const message = ({ id, text }) => <li key={id}>{text}</li>;
+const message = ({ id, text, user: { username } }, createdAt) => (
+  <Comment key={`message-${id}`}>
+    <Comment.Avatar src="https://react.semantic-ui.com/images/avatar/small/jenny.jpg" />
+    <Comment.Content>
+      <Comment.Author as="a">{username}</Comment.Author>
+      <Comment.Metadata>
+        <div>{createdAt.toString()}</div>
+      </Comment.Metadata>
+      <Comment.Text>{text}</Comment.Text>
+      <Comment.Actions>
+        <Comment.Action>Reply</Comment.Action>
+      </Comment.Actions>
+    </Comment.Content>
+  </Comment>
+);
 
 const MessageContainer = ({ data: { loading, messages } }) => {
   if (loading) {
@@ -11,7 +26,15 @@ const MessageContainer = ({ data: { loading, messages } }) => {
   }
   return (
     <Messages>
-      <ul className="message-list">{messages.map(msg => message(msg))}</ul>
+      <Comment.Group>
+        <Header as="h3" dividing>
+          Messages
+        </Header>
+        {messages.map((msg) => {
+          const createdAt = new Date(parseInt(msg.created_at, 10));
+          return message(msg, createdAt);
+        })}
+      </Comment.Group>
     </Messages>
   );
 };
@@ -20,9 +43,12 @@ const messagesQuery = gql`
   query($channelId: Int!) {
     messages(channelId: $channelId) {
       text
-      userId
-      channelId
+      user {
+        id
+        username
+      }
       id
+      created_at
     }
   }
 `;
